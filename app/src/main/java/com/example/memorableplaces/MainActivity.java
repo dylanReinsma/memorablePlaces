2e2074376b7d97f1a48594ba486b99c74a6f4bb8
dylanreinsma <dylanreinsma@gmail.com>
2019-07-29 11:27:50 -0700

    removed onClick method from main xml (causing crash) added more code to map activity, still WIP

--- app/src/main/java/com/example/memorableplaces/MainActivity.java
+++ app/src/main/java/com/example/memorableplaces/MainActivity.java
@@ -6,11 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
